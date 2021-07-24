fedf782d4674369c7438816cfa15c7a1a32f2636
Ricardo R <rretamal.dev@gmail.com>
2021-07-24 10:02:11 -0400

    fix(main): fixes to files and layouts

---
+++ ProyectoDesafio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProyectoDesafio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
